b71431ba266c584ab81b6bc57b736df40822922e
sayunanasan <99886308+sayunana@users.noreply.github.com>
2025-09-23 00:02:16 +0900

    Init

---
+++ Assets/sayunana/sayunanaBase/Translate.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace sayunanaBase.Helper
+{
+    public partial class Translate
+    {
+        public enum Language
+        {
+            ja = 0,
+            en
+        }
+
+        public static Language SystemLanguage = Language.ja;
+        private static Dictionary<string, Dictionary<string, string>> _loadLanguageText;
+
+        public static string TranslateText(string projectName, Language language, string key)
+        {
+            if (_loadLanguageText == null)
+            {
+                TranslateTextLoad(projectName, language);
+            }
+
+            if (!_loadLanguageText.TryGetValue(projectName, out var translateDic))
+            {
+                return $"未設定 : {key}";
+            }
+
+            return translateDic.ContainsKey(key) ? translateDic[key] : key;
+        }
+
+        private static void TranslateTextLoad(string projectName, Language language)
+        {
+            var loadLanguageFile = Resources.Load<TextAsset>($"{projectName}-{language}").ToString();
+            _loadLanguageText.Add(projectName, Deserialize(loadLanguageFile));
+        }
+
+        private static Dictionary<string, string> Deserialize(string json)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            json = json.Replace("{", "").Replace("}", "");
+
+            var columns = json.Split(",\r\n");
+
+            foreach (var column in columns)
+            {
+                var t = column.Split(":");
+                var key = t[0];
+                var value = t[1];
+                string pattern = $@"{Regex.Escape("\"")}(.*?){Regex.Escape("\"")}";
+                var keyOut = Regex.Match(key, pattern);
+                var valueOut = Regex.Match(value, pattern);
+                dic.Add(keyOut.Groups[1].Value, valueOut.Groups[1].Value);
+            }
+
+            return dic;
+        }
+    }
+}
